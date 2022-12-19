cccf36fb466cb8776aa9c4e69ca33d7da799ed12
guo <484764968@qq.com>
2022-12-19 11:22:23 +0800

    项目初始化--引入vant和icon

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div id="app">
-
+    <h4>黑马</h4>
+    <div>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+    </div>
   </div>
 </template>
+<script>
 
+</script>
 <style lang="less">
 
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import '@/styles/index.less'
-Vue.config.productionTip = false
 
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 加载全局样式
+import '@/styles/index.less'
+Vue.use(Vant)
+
+Vue.config.productionTip = false
 new Vue({
   router,
   store,
